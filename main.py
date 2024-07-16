e0028f3f556cd7d7cf7f6e70f881201c251ded83
Aleksandr <Aleksandr260206@yandex.ru>
2024-07-16 13:30:22 +0300

    Создание, разработка бота

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import asyncio
+import logging
 
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
+from config_data.config import Config, load_config
+from handlers import user_handlers
+from handlers import other_handlers
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-10s'
+        '[%(asctime)s] - %(name)s - %(message)s'
+    )
+
+    logger.info('Starting bot')
+
+    config: Config = load_config()
+
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
